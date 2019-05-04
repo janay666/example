3127b6813bbb1517830f1e399fb5d0d84a5864a5
janus <janus@DESKTOP-GRCSERB>
2019-05-04 20:08:42 +0200

    demo app

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+    //dfs fsdf sdf sdfsfd 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
